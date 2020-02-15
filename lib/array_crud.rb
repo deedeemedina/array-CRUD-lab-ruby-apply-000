227191eba9145ada6b5342754db90f457268c8a2
deedee medina <deedee.medinacorp@gmail.com>
2020-02-15 20:41:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-    create_an_array.array.pop("arrays!")
+    create_an_array.element.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
