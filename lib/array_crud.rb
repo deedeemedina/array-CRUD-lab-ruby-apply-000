06e1038118dffb93e5282a89ec387ec2fae76351
deedee medina <deedee.medinacorp@gmail.com>
2020-02-15 19:58:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,8 +13,7 @@ end
 
 
 def add_element_to_start_of_array(array, element)
-    add_element_to_start_of_array.unshift("wow")
-  end
+    create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
